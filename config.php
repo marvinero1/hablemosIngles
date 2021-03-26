7b68281ea5d8f426bc6e4b68da590414ffaf4773
marvinero1 <marvinero@gmail.com>
2021-03-25 23:01:42 -0400

    asd

--- config.php
+++ config.php
@@ -1,15 +1,11 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'u489850489_root');
-// define('DB_PASSWORD', '5[eoMEP6gFaM');
-// define('DB_NAME', 'u489850489_hablemos');
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'register');
- 
+define('DB_USERNAME', 'u489850489_root');
+define('DB_PASSWORD', '5[eoMEP6gFaM');
+define('DB_NAME', 'u489850489_hablemos');
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
